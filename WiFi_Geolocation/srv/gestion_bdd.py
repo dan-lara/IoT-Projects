153bda90dfe31a2a9d18e52da3d7fffb76c9d7c5
Daniel Ferreira Lara <d2021003661@unifei.edu.br>
2024-10-24 20:33:39 +0200

    Refactor database persistence functions and add support for SQLite
    This commit refactors the `persist_data` function in the `gestion_bdd.py` file to improve database persistence. It introduces support for SQLite as an alternative to PostgreSQL. The function now takes a `db_type` parameter to determine the type of database to use. Additionally, the commit adds a new function `persist_data_sqlite` to handle data insertion into the SQLite database.
    
    The changes also include updates to the `mqtt_server.py` file. The `on_message` function now calls the `get_location_df` function to fetch location data from a CSV file and process it. The commit also includes minor code cleanup and debugging enhancements.
    
    These changes aim to improve the overall performance and flexibility of the codebase.

--- WiFi_Geolocation/srv/gestion_bdd.py
+++ WiFi_Geolocation/srv/gestion_bdd.py
@@ -1,4 +1,98 @@
+import paho.mqtt.client as mqtt
 import psycopg2
+import sqlite3
 import json
 import os
 from dotenv import load_dotenv
+
+#POSTGRES
+POSTGRES_HOST = os.getenv('POSTGRES_HOST')
+POSTGRES_PORT = os.getenv('POSTGRES_PORT')
+POSTGRES_DATABASE = os.getenv('POSTGRES_DATABASE')
+POSTGRES_USER = os.getenv('POSTGRES_USER')
+POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
+
+# Si on veux utiliser SQLite, il faut utiliser la fonction persist_data_sqlite
+#SQLite
+DB_FILE = 'data/geoloc_data.db'
+
+
+def persist_data(device_name, lat, lon, wifi_json, db_type='sqlite'):
+    """Insère les données reçues dans la base de données"""
+    if db_type == 'sqlite':
+        persist_data_sqlite(device_name, lat, lon, wifi_json)
+    elif db_type == 'postgres':
+        persist_data_postgres(device_name, lat, lon, wifi_json)
+    else:
+        print("Type de base de données inconnu.")
+
+
+def persist_data_postgres(device_name, lat, lon, wifi_json):
+    """Insère les données reçues dans la base de données PostgreSQL"""
+    try:
+        # Connexion à la base de données PostgreSQL
+        conn = psycopg2.connect(
+            host=POSTGRES_HOST,
+            port=POSTGRES_PORT,
+            dbname=POSTGRES_DATABASE,
+            user=POSTGRES_USER,
+            password=POSTGRES_PASSWORD
+        )
+        cursor = conn.cursor()
+
+        # Création de la table si elle n'existe pas
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS locations (
+                id SERIAL PRIMARY KEY,
+                device_name TEXT,
+                timestamp TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+                latitude REAL,
+                longitude REAL,
+                wifi_networks JSON
+            )
+        ''')
+
+        # Insertion des données dans la table
+        cursor.execute('''
+            INSERT INTO locations (device_name, latitude, longitude, wifi_networks)
+            VALUES (%s, %s, %s, %s)
+        ''', (device_name, lat, lon, wifi_json))
+
+        # Validation de l'insertion dans la base de données
+        conn.commit()
+        cursor.close()
+        conn.close()
+
+        print(f"Les données pour {device_name} ont été enregistrées dans la base de données PostgreSQL.")
+    
+    except Exception as e:
+        print(f"Erreur lors de l'enregistrement dans la base de données PostgreSQL: {e}")
+
+def persist_data_sqlite(device_name, lat, lon, wifi_json):
+    """Insère les données reçues dans la base de données SQLite"""
+    try:
+        # Connexion à la base de données SQLite
+        conn = sqlite3.connect(DB_FILE)
+        c = conn.cursor()
+
+        # Création de la table si elle n'existe pas
+        c.execute('''CREATE TABLE IF NOT EXISTS locations
+                    (id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                    device_name TEXT, 
+                    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP, 
+                    latitude REAL, 
+                    longitude REAL,
+                    wifi_networks TEXT)''')  # Colonne pour le JSON des réseaux WiFi
+        
+        # Insertion des données dans la table
+        c.execute("INSERT INTO locations (device_name, latitude, longitude, wifi_networks) VALUES (?, ?, ?, ?)",
+                (device_name, lat, lon, wifi_json))
+        
+        # Validation de l'insertion dans la base de données
+        conn.commit()
+        conn.close()
+    
+        print(f"Les données pour {device_name} ont été enregistrées dans la base de données locale SQLite.")
+    
+    except Exception as e:
+        print(f"Erreur lors de l'enregistrement dans la base de données locale SQLite: {e}")
