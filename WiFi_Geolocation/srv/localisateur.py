226922e7be9f74f8a59b932f0888749df34d4afe
Daniel Ferreira Lara <d2021003661@unifei.edu.br>
2024-10-26 20:16:47 +0200

    Refactor Localisateur

--- WiFi_Geolocation/srv/localisateur.py
+++ WiFi_Geolocation/srv/localisateur.py
@@ -1,61 +1,80 @@
 import pandas as pd
+from io import StringIO
 import json
+import os
+
+DEBUG = False
 
 def read_DB_csv_file(csv_file):
-    # Load CSV file and parse into a pandas DataFrame
-    # Charger le fichier CSV et l'analyser dans un DataFrame pandas
-    csv_file = 'BDD.csv'
-    df = pd.read_csv(csv_file, delimiter=';', encoding='ISO-8859-1')
-    # df = df.dropna()
+    """
+    Charger un fichier CSV (d'un chemin local ou d'une URL GitHub) et l'analyser dans un DataFrame pandas.
+    """
+    # Vérifier si l'entrée est une URL ou un chemin de fichier local
+    if csv_file.startswith('http://') or csv_file.startswith('https://') or os.path.isfile(csv_file):
+        df = pd.read_csv(csv_file, delimiter=';', encoding='ISO-8859-1')
+    else:
+        # Lever une exception si le chemin du fichier n'existe pas
+        raise FileNotFoundError(f"Le chemin du fichier '{csv_file}' n'existe pas.")
     return df
 
 def read_WiFi_json_file(json_file):
-    # Define the structure of the JSON output
-    # Définir la structure de sortie JSON
+    # Lire le fichier JSON
     with open(json_file, 'r') as f:
         wifi_data = json.load(f)
+    
+    # Initialiser une liste pour stocker les réseaux WiFi
     wifi_networks = []
 
-    # Iterate through each row in the DataFrame and create a JSON-like structure
-    # Itérer à travers chaque ligne du DataFrame pour créer une structure JSON
+    # Itérer à travers chaque élément dans les données JSON pour créer une structure de type JSON
     for element in wifi_data.get("Networks", []):
         network = {
-            'MAC': element.get('MAC'),
-            # 'SSID': element.get('SSID'),
-            'RSSI': element.get('RSSI'),
+            'MAC': element.get('MAC'),  # Adresse MAC du réseau WiFi
+            'RSSI': element.get('RSSI'),  # Puissance du signal du réseau WiFi
         }
         wifi_networks.append(network)
     
+    # Convertir la liste des réseaux WiFi en une chaîne JSON formatée
     wifi_json = json.dumps(wifi_networks, indent=4)
     return wifi_json
 
 def fetch_locations(df, wifi_json):
-    # Load the JSON data into a DataFrame
     # Charger les données JSON dans un DataFrame
-
-    wifi_df = pd.read_json(wifi_json)
-    print(wifi_df)
-    # Merge the WiFi DataFrame with the location DataFrame based on the MAC address
-    # Fusionner le DataFrame WiFi avec le DataFrame de localisation en fonction de l'adresse MAC
+    if DEBUG:
+        print(type(wifi_json))
+    wifi_df = pd.read_json(StringIO(wifi_json))
+    if DEBUG:
+        print(wifi_df)  
+    
+    # Définir les colonnes du DataFrame de localisation
     df.columns = ['MAC', 'SSID', 'RSSI', 'Latitude', 'Longitude', 'Altitude', 'Acuracy', 'Type']
+    
+    # Fusionner le DataFrame WiFi avec le DataFrame de localisation en fonction de l'adresse MAC
     merged_df = pd.merge(wifi_df, df, on='MAC', how='inner')
-    print("Merged DataFrame: \n", merged_df)
-    print(merged_df.size)
-    # if merged_df.empty:# or merged_df.size < 30:
-    #     return search_api()
+    if DEBUG:
+        print("Merged DataFrame: \n", merged_df)
+        print(merged_df.size)
+    
+    # Renommer la colonne RSSI pour éviter les conflits
     merged_df.rename(columns={'RSSI_x': 'RSSI'}, inplace=True)
+    
+    # Supprimer les colonnes inutiles
     merged_df.drop(['RSSI_y', 'Type'], axis=1, inplace=True)
+    
+    # Convertir les colonnes de localisation en type float
     merged_df[['Latitude', 'Longitude', 'Altitude', 'Accuracy']] = merged_df[['Latitude', 'Longitude', 'Altitude', 'Acuracy']].astype(float)
+    
+    # Convertir la colonne RSSI en type int
     merged_df['RSSI'] = merged_df['RSSI'].astype(int)
+    
     return merged_df
 
-def search_api():
-    # Placeholder for API call to fetch location data
-    # Placeholder pour l'appel API pour obtenir les données de localisation
-    return None
+if DEBUG:
+    # bdd = read_DB_csv_file('data/BDD.csv')
+    bdd = read_DB_csv_file('https://github.com/dan-lara/IoT-Projects/raw/refs/heads/master/WiFi_Geolocation/srv/data/BDD.csv')
+    print("BDD CSV:", bdd)
 
-bdd = read_DB_csv_file('BDD.csv')
-wifi = read_WiFi_json_file('wifi_data.json')
-print("WiFi JSON: \n", wifi)
-location_df = fetch_locations(bdd, wifi)
-print("Location DataFrame: \n", location_df)
+    wifi = read_WiFi_json_file('data/wifi_data.json')
+    print("WiFi JSON: \n", wifi)
+
+    location_df = fetch_locations(bdd, wifi)
+    print("DataFrame de localisation: \n", location_df)
