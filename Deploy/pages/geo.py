aed69d0650543a0a15869e9c783c38ec86eadbde
Daniel Ferreira Lara <d2021003661@unifei.edu.br>
2024-10-26 21:12:18 +0200

    Refactor code to remove unused Streamlit files and dependencies

---
+++ Deploy/pages/geo.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def run():
+    st.title("Geoloc")
+    st.write("This is the Geoloc app.")
+    st.write("It's still under development.")
---
+++ Deploy/pages/temp.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def run():
+    st.title("LoRa Temp")
+    st.write("This is the LoRa Temp app.")
+    st.write("It's still under development.")
