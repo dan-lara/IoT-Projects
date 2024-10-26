801ff49b1f9b730c2c8e3123ac6c421ffa1b23c1
Daniel Ferreira Lara <d2021003661@unifei.edu.br>
2024-10-26 21:37:02 +0200

    Refactor code to update Streamlit app layout and add login functionality

--- Deploy/streamlit_app.py
+++ Deploy/streamlit_app.py
@@ -1,18 +1,10 @@
 import streamlit as st
-st.logo("images/poly_logo.jpeg", icon_image="images/logo.jpeg")
-# try:
-#     from WiFi_Geolocation.srv.app import geoloc_main as geoloc_app  # Certifique-se de que 'app.py' possui uma função 'run'
-# except Exception as e:
-#     st.error("Módulo WiFi_Geolocation não encontrado. Verifique o caminho e a estrutura.")
-#     print(e)
-# tp2 = st.Page(geoloc_app, title="Geolocation")
 
-# try:
-#     from LoRaTemp.srv.app import temp_hum_main as temp_app  # Certifique-se de que 'app.py' possui uma função 'run'
-# except Exception as e:
-#     st.error("Módulo WiFi_Geolocation não encontrado. Verifique o caminho e a estrutura.")
-#     print(e)
-# tp3 = st.Page(temp_app, title="Temperature")
+st.set_page_config(page_title="Data manager", page_icon=":shark:", layout="wide")
+
+st.title("Data manager")
+st.logo("images/poly_logo.jpeg", icon_image="images/logo.jpeg")
+
 tp2 = st.Page(
     "pages/geo.py",
     title="Request 1",
@@ -23,10 +15,11 @@ tp3 = st.Page(
     title="Request 2",
     icon=":material/help:",
 )
-pg = st.navigation([tp2, tp3]
-)
-st.set_page_config(page_title="Data manager", page_icon=":shark:", layout="wide")
-st.sidebar.title("Data manager")
-st.title("Data manager")
-st.write("This application is a data manager for the IoT devices.")
+def login():
+    st.sidebar.title("Data manager")
+    st.header("Log in")
+    st.write("This application is a data manager for the IoT devices.")
+
+pg = st.navigation([st.Page(login),tp2, tp3])
+
 pg.run()
