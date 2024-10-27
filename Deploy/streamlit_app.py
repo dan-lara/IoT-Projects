70239834fd0596b7dd2ce3663e613736d812e800
Daniel Ferreira Lara <d2021003661@unifei.edu.br>
2024-10-27 13:54:20 +0100

    Refactor code to update Streamlit app entry point function name

--- Deploy/streamlit_app.py
+++ Deploy/streamlit_app.py
@@ -1,25 +1,29 @@
 import streamlit as st
 
-st.set_page_config(page_title="Data manager", page_icon=":shark:", layout="wide")
+def main():
+    st.set_page_config(page_title="Data manager", page_icon=":shark:", layout="wide")
 
-st.title("Data manager")
-st.logo("images/poly_logo.png", icon_image="images/logo.png")
+    st.title("Data manager")
+    st.logo("images/poly_logo.png", icon_image="images/logo.png")
 
-tp2 = st.Page(
-    "pages/geo.py",
-    title="Geolocation",
-    icon=":world:",
-)
-tp3 = st.Page(
-    "pages/temp.py",
-    title="Temperature",
-    icon=":material/help:",
-)
-def login():
-    st.sidebar.title("Data manager")
-    st.header("Log in")
-    st.write("This application is a data manager for the IoT devices.")
+    tp2 = st.Page(
+        "pages/geo.py",
+        title="Geolocation",
+        icon=":world:",
+    )
+    tp3 = st.Page(
+        "pages/temp.py",
+        title="Temperature",
+        icon=":material/help:",
+    )
+    def login():
+        st.sidebar.title("Data manager")
+        st.header("Log in")
+        st.write("This application is a data manager for the IoT devices.")
 
-pg = st.navigation([st.Page(login),tp2, tp3])
+    pg = st.navigation([st.Page(login),tp2, tp3])
 
-pg.run()
+    pg.run()
+
+if __name__ == "__main__":
+    main()
