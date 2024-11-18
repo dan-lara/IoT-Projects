ee88c41b33b2159524ca43a6f5df5cb93f3ad5cd
Daniel Ferreira Lara <d2021003661@unifei.edu.br>
2024-11-18 03:53:28 +0100

    Create The FrontendAPI

---
+++ Logement_Eco/api/api_front.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+
+from routers import meteo, facture
+
+# Métadonnées pour la documentation OpenAPI
+tags_metadata = [
+    {"name": "Facture", "description": "Routes pour gérer les factures"},
+    {"name": "Meteo", "description": "Routes pour obtenir les données météorologiques"},
+]
+
+# Création de l'application FastAPI avec des informations de base et des tags pour la documentation
+app = FastAPI(
+    title="Logement Éco-Responsable Frontend",
+    description="Application pour la gestion éco-responsable des logements",
+    version="0.1.0",
+    openapi_tags=tags_metadata
+)
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+app.include_router(facture.router, tags=["Facture"], prefix="/facture")
+app.include_router(meteo.router, tags=["Meteo"], prefix="/meteo")
