9198dccf29147de6ac076752e66db43b1e3f885f
Daniel Ferreira Lara <d2021003661@unifei.edu.br>
2024-11-18 03:52:08 +0100

    Change Backend API to api_back

--- Logement_Eco/api/api_back.py
+++ Logement_Eco/api/api_back.py
@@ -3,7 +3,7 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 import random
 
-from routers import ville, logement#, adresse, logement, piece, type_capteur, capteur, mesure, type_facture, facture
+from routers.bdd import ville, adresse, logement, piece, type_capteur, capteur, mesure, type_facture, facture
 
 # Métadonnées pour la documentation OpenAPI
 tags_metadata = [
@@ -21,7 +21,7 @@ tags_metadata = [
 
 # Création de l'application FastAPI avec des informations de base et des tags pour la documentation
 app = FastAPI(
-    title="Logement Éco-Responsable",
+    title="Logement Éco-Responsable Backend",
     description="Application pour la gestion éco-responsable des logements",
     version="0.1.0",
     openapi_tags=tags_metadata
@@ -29,28 +29,28 @@ app = FastAPI(
 templates = Jinja2Templates(directory="templates")
 
 # Rota principal para testar a renderização do template com gráfico
-@app.get("/", response_class=HTMLResponse)
-async def read_home(request: Request):
-    # Dados simulados para enviar ao template
-    data = {
-        "sensor": "Temperature",
-        "value": f"{random.randint(20, 30)}°C",  # Gera um valor aleatório para simular a temperatura
-        "status": "Active",
-        "consumption": [
-            ["Eau", random.randint(20, 50)],
-            ["Eléctricite", random.randint(30, 60)],
-            ["Déchets", random.randint(10, 30)]
-        ]
-    }
-    return templates.TemplateResponse("home.html", {"request": request, "data": data})
+# @app.get("/", response_class=HTMLResponse, tags=["Racine"])
+# async def read_home(request: Request):
+#     # Dados simulados para enviar ao template
+#     data = {
+#         "sensor": "Temperature",
+#         "value": f"{random.randint(20, 30)}°C",  # Gera um valor aleatório para simular a temperatura
+#         "status": "Active",
+#         "consumption": [
+#             ["Eau", random.randint(20, 50)],
+#             ["Eléctricite", random.randint(30, 60)],
+#             ["Déchets", random.randint(10, 30)]
+#         ]
+#     }
+#     return templates.TemplateResponse("home.html", {"request": request, "data": data})
 
 # Inclusion des routeurs avec des préfixes et des tags spécifiques
 app.include_router(ville.router, tags=["Ville"], prefix="/ville")
-# app.include_router(adresse.router, tags=["Adresse"], prefix="/adresse")
+app.include_router(adresse.router, tags=["Adresse"], prefix="/adresse")
 app.include_router(logement.router, tags=["Logement"], prefix="/logement")
-# app.include_router(piece.router, tags=["Piece"], prefix="/piece")
-# app.include_router(type_capteur.router, tags=["Type_Capteur"], prefix="/type_capteur")
-# app.include_router(capteur.router, tags=["Capteur"], prefix="/capteur")
-# app.include_router(mesure.router, tags=["Mesure"], prefix="/mesure")
-# app.include_router(type_facture.router, tags=["Type_Facture"], prefix="/type_facture")
-# app.include_router(facture.router, tags=["Facture"], prefix="/facture")
+app.include_router(piece.router, tags=["Piece"], prefix="/piece")
+app.include_router(type_capteur.router, tags=["Type_Capteur"], prefix="/type_capteur")
+app.include_router(capteur.router, tags=["Capteur"], prefix="/capteur")
+app.include_router(mesure.router, tags=["Mesure"], prefix="/mesure")
+app.include_router(type_facture.router, tags=["Type_Facture"], prefix="/type_facture")
+app.include_router(facture.router, tags=["Facture"], prefix="/facture")
