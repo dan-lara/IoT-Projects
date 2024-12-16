d7d9b1b5a861e2cea4a7411fe8d36961509aafb0
Daniel Ferreira Lara <d2021003661@unifei.edu.br>
2024-12-16 03:58:59 +0100

    Refactor API health check route: replace commented home route with a simple health check endpoint returning status

--- Logement_Eco/api/api_back.py
+++ Logement_Eco/api/api_back.py
@@ -4,9 +4,11 @@ from fastapi.templating import Jinja2Templates
 import random
 
 from routers.bdd import ville, adresse, logement, piece, type_capteur, capteur, mesure, type_facture, facture
+# from routers import autentication
 
 # Métadonnées pour la documentation OpenAPI
 tags_metadata = [
+    # {"name": "Auth", "description": "Routes pour l'authentification et l'autorisation"},
     {"name": "Racine", "description": "Routes de base"},
     {"name": "Ville", "description": "Routes pour gérer les villes"},
     {"name": "Adresse", "description": "Routes pour gérer les adresses"},
@@ -27,22 +29,10 @@ app = FastAPI(
     openapi_tags=tags_metadata
 )
 templates = Jinja2Templates(directory="templates")
-
-# Rota principal para testar a renderização do template com gráfico
-# @app.get("/", response_class=HTMLResponse, tags=["Racine"])
-# async def read_home(request: Request):
-#     # Dados simulados para enviar ao template
-#     data = {
-#         "sensor": "Temperature",
-#         "value": f"{random.randint(20, 30)}°C",  # Gera um valor aleatório para simular a temperatura
-#         "status": "Active",
-#         "consumption": [
-#             ["Eau", random.randint(20, 50)],
-#             ["Eléctricite", random.randint(30, 60)],
-#             ["Déchets", random.randint(10, 30)]
-#         ]
-#     }
-#     return templates.TemplateResponse("home.html", {"request": request, "data": data})
+# Route principale pour tester l'api
+@app.get("/", tags=["Racine"])
+def health_check():
+    return {"status": "OK"}
 
 # Inclusion des routeurs avec des préfixes et des tags spécifiques
 app.include_router(ville.router, tags=["Ville"], prefix="/ville")
@@ -54,3 +44,4 @@ app.include_router(capteur.router, tags=["Capteur"], prefix="/capteur")
 app.include_router(mesure.router, tags=["Mesure"], prefix="/mesure")
 app.include_router(type_facture.router, tags=["Type_Facture"], prefix="/type_facture")
 app.include_router(facture.router, tags=["Facture"], prefix="/facture")
+# app.include_router(autentication.router, tags=["Auth"], prefix="/auth")
