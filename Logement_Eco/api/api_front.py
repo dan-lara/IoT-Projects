7d21953183e7549fce731ccb94c2b9a38458be38
Daniel Ferreira Lara <d2021003661@unifei.edu.br>
2024-12-16 03:58:02 +0100

    Refactor API structure: separate routers for partial meteo and facture, update FastAPI app configuration, and add new HTML response routes

--- Logement_Eco/api/api_front.py
+++ Logement_Eco/api/api_front.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
-from routers import meteo, facture, capteur
+from routers.partial import meteo, facture
+from routers.front import capteur, index
 
 # Métadonnées pour la documentation OpenAPI
 tags_metadata = [
-    {"name": "Facture", "description": "Routes pour gérer les factures"},
-    {"name": "Meteo", "description": "Routes pour obtenir les données météorologiques"},
+    {"name": "Ancien", "description": "Routes anciennes les factures et obtenir les données météorologiques"},
+    {"name": "Frontend", "description": "Routes pour le frontend de l'application"}
 ]
 
 # Création de l'application FastAPI avec des informations de base et des tags pour la documentation
@@ -18,6 +19,8 @@ app = FastAPI(
 )
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-app.include_router(facture.router, tags=["Facture"], prefix="/facture")
-app.include_router(meteo.router, tags=["Meteo"], prefix="/meteo")
+app.include_router(facture.router, tags=["Ancien"], prefix="/facture")
+app.include_router(meteo.router, tags=["Ancien"], prefix="/meteo")
+
 app.include_router(capteur.router, tags=["Capteur"], prefix="/capteur")
+app.include_router(index.router, tags=["Frontend"], prefix="")
